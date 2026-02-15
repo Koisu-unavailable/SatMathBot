ad8f05a504ba1e654a1da5eb71947cf7dab84e47
Jamil Alauusa <jalau@Jamils-MacBook-Air-2.local>
2026-02-14 21:26:11 -0800

    tmp
     Please enter the commit message for your changes. Lines starting

--- satmathbot/genmath/algebra.py
+++ satmathbot/genmath/algebra.py
@@ -1,10 +1,21 @@
+import random as rdm
+import utils
 import satmathbot.genmath.ProblemGenerator as ProbGen
-import satmathbot.utils
+import
+from utils.tree import NumberNode, OperationNode, VariableNode
+
 
 class AlgebraGen(ProbGen.ProblemGenerator):
     def __init__(self):
         super().__init__()
-    
+
     def generate_problem(self):
         """Returns the path to an image of the generated problem"""
-        return satmathbot.utils.render_latex(r'\frac{a}{b}')
+        solution = rdm.randint(0, 100)
+        NumberNode(solution, None)
+        return utils.render_latex(r"\frac{a}{b}")
+
+
+if __name__ == "__main__":
+    gen = AlgebraGen()
+    gen.generate_problem()
